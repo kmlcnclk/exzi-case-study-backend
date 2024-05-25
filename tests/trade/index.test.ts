fec645c7939128ea98152aeddf0a47d5ce153f18
Kamilcan Çelik <kamilcancelik5757@gmail.com>
2024-05-25 10:30:12 +0300

    Writing trade and wallet tests
    Writing tests for /trade/buy-and-sale and /wallet/withdraw.

--- tests/trade/index.test.ts
+++ tests/trade/index.test.ts
@@ -14,5 +14,25 @@ describe("Trade API", () => {
     });
   });
 
-  describe("GET /trade/buy-and-sale", () => {});
+  describe("POST /trade/buy-and-sale", () => {
+    it("User should not be able to buy and sale without enough balance", async () => {
+      const accessToken =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYWFhYSIsImVtYWlsIjoiYUBnbWFpbC5jb20iLCJfaWQiOiI2NjRmMjJkZDBlZTdiYmYwNmZiNmRhMTMiLCJjcmVhdGVkQXQiOiIyMDI0LTA1LTIzVDExOjA1OjAxLjE5N1oiLCJpYXQiOjE3MTY1NTQ3ODAsImV4cCI6MTcxNzE1OTU4MH0.0utJAiA_pcQdJcKluLVHjF0-0Gi2WjNeSNU6U8vtAlY";
+
+      const response = await request("http://localhost:8000")
+        .post("/trade/buy-and-sell")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({
+          tokens: ["usdt", "eth"],
+          amount: "0.000001",
+          network: "bsc",
+        });
+
+      expect(response.status).toBe(500);
+      expect(response.body).toHaveProperty("error");
+      expect(response.body.error).toEqual(
+        "Your balance is not enough to make this transaction"
+      );
+    });
+  });
 });
--- tests/wallet/index.test.ts
+++ tests/wallet/index.test.ts
@@ -45,5 +45,28 @@ describe("Wallet API", () => {
     });
   });
 
-  describe("GET /trade/withdraw", () => {});
+  describe("POST /wallet/withdraw", () => {
+    it("User should not be able to withdraw", async () => {
+      const accessToken =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYWFhYSIsImVtYWlsIjoiYUBnbWFpbC5jb20iLCJfaWQiOiI2NjRmMjJkZDBlZTdiYmYwNmZiNmRhMTMiLCJjcmVhdGVkQXQiOiIyMDI0LTA1LTIzVDExOjA1OjAxLjE5N1oiLCJpYXQiOjE3MTY1NTQ3ODAsImV4cCI6MTcxNzE1OTU4MH0.0utJAiA_pcQdJcKluLVHjF0-0Gi2WjNeSNU6U8vtAlY";
+
+      const response = await request("http://localhost:8000")
+        .post("/wallet/withdraw")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({
+          amount: "0.000000001",
+          to: "0x177d84631999414480E833Ae7a1e6235ce5bdB3f",
+          network: "ethereum",
+          token: "eth",
+        });
+
+      expect(response.status).toBe(500);
+      expect(response.body).toHaveProperty("error");
+      expect(response.body.error).toHaveProperty("name");
+      expect(response.body.error).toHaveProperty("message");
+
+      expect(response.body.error.name).toEqual("Internal Server Error");
+      expect(response.body.error.message).toEqual("Withdraw is not successful");
+    });
+  });
 });
